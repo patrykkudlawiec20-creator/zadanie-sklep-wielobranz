cf804c259a8766799909598f34243d3075be8faf
patrykkudlawiec20-creator <patrykkudlawiec20@gmail.com>
2026-02-20 19:04:58 +0100

    part 2

--- utils/produkty.py
+++ utils/produkty.py
@@ -17,7 +17,29 @@ except Exception as e:
 
 
 class Produkty():
-    pass
+    def __init__(self):
+        db = client["Sklep_filmy"]
+        self.products = db['products']
+
+    def show(self, category):
+        categorical_products = list(self.products.find({"category": category}))
+
+        name_list = [x['name'] for x in categorical_products]
+        price_list = [y['price'] for y in categorical_products]
+        link_list = [z['image'] for z in categorical_products]
+
+
+        return name_list, price_list, link_list
+    
+    def show_all(self):
+
+        products = list(self.products.find())
+        name_list = [x['name'] for x in products]
+        price_list = [y['price'] for y in products]
+        link_list = [z['image'] for z in products]
+
+
+        return name_list, price_list, link_list
 
 
 
